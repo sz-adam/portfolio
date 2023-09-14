dcec6b42a40ca9450870ccce21f1ea931162e4f2
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-09-14 09:40:22 +0200

    activeNavbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,79 @@
-import React, { useContext, useEffect, useState} from 'react'
-import { LanguageContext } from '../context/LanguageContext'
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { LanguageContext } from '../context/LanguageContext';
+import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import AnimatedMotion from '../framerMotion/AnimatedMotion';
 import About from '../pages/About';
 import Portfolio from '../pages/Portfolio';
 import Skills from '../pages/Skills';
 
 const Navbar = () => {
+  const { language } = useContext(LanguageContext);
+  const i18n = require('../utils/i18n');
+  const [activeItem, setActiveItem] = useState(getInitialActiveItem());
 
-    const { language } = useContext(LanguageContext);
-    const i18n = require('../utils/i18n');
-    const [activeItem, setActiveItem] = useState(getInitialActiveItem());
+  useEffect(() => {
+    window.addEventListener('hashchange', handleHashChange);
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange); 
+    };
+  }, []);
 
-    useEffect(() => {
-        setActiveItem(getInitialActiveItem());
-    }, []);
+  function handleHashChange() {   
+    setActiveItem(getInitialActiveItem());
+  }
 
-   
-    function getInitialActiveItem() {
-        const pathname = window.location.pathname;
-        if (pathname === '/') return 'About';
-        if (pathname === '/skills') return 'Skills';
-        if (pathname === '/portfolio') return 'Portfolio';
-        return 'About'; 
-    }
+  function getInitialActiveItem() {
+    const hash = window.location.hash;
+    if (hash === '#/') return 'About';
+    if (hash === '#/skills') return 'Skills';
+    if (hash === '#/portfolio') return 'Portfolio';
+    return ''; 
+  }
 
-    return (
-        <Router>
-            <AnimatedMotion animationName="navbarAnimatio">
-                <div className="flex justify-center p-5">
-                    <div>{activeItem}</div>
-                    {activeItem!== 'About' && (
-                    <Link to="/" className={"p-2 hover:border-b-2 font-bold"}>
-                        <span onClick={()=>setActiveItem('About')}>{i18n.text(language, i18n.MAP['navbar-home'])}</span>
-                    </Link>
-                    )}
-                      {activeItem!== 'Skills' && (
-                    <Link to="/skills" className={"p-2 hover:border-b-2 font-bold "}>
-                        <span onClick={()=>setActiveItem('Skills')}>{i18n.text(language, i18n.MAP['navbar-skills'])}</span>
-                    </Link>
-                    )}
-                      {activeItem!== 'Portfolio' && (
-                    <Link to="/portfolio" className={"p-2 hover:border-b-2 font-bold "}>
-                        <span onClick={()=>setActiveItem('Portfolio')}>{i18n.text(language, i18n.MAP['navbar-portfolio'])}</span>
-                    </Link>
-                    )}
-                </div>
+  return (
+    <Router>
+      <AnimatedMotion animationName="navbarAnimatio">
+        <div className="flex justify-center p-5">
+          <Link
+            to="/"
+            className={`p-2 hover:border-b-2 font-bold ${
+              activeItem === 'About' ? 'border-b-2 text-green-500' : ''
+            }`}
+          >
+            <span onClick={() => window.location.hash = '#/About'}>
+              {i18n.text(language, i18n.MAP['navbar-home'])}
+            </span>
+          </Link>
+          <Link
+            to="/skills"
+            className={`p-2 hover:border-b-2 font-bold ${
+              activeItem === 'Skills' ? 'border-b-2 text-green-500' : ''
+            }`}
+          >
+            <span onClick={() => window.location.hash = '#/skills'}>
+              {i18n.text(language, i18n.MAP['navbar-skills'])}
+            </span>
+          </Link>
+          <Link
+            to="/portfolio"
+            className={`p-2 hover:border-b-2 font-bold ${
+              activeItem === 'Portfolio' ? 'border-b-2 text-green-500' : ''
+            }`}
+          >
+            <span onClick={() => window.location.hash = '#/portfolio'}>
+              {i18n.text(language, i18n.MAP['navbar-portfolio'])}
+            </span>
+          </Link>
+        </div>
 
-                <Routes>
-                    <Route path="/" exact element={<About />} />
-                    <Route path="/skills" element={<Skills />} />
-                    <Route path="/portfolio" element={<Portfolio />} />
-                </Routes>
-            </AnimatedMotion>
-        </Router>
-    );
+        <Routes>
+          <Route path="/" exact element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+      </AnimatedMotion>
+    </Router>
+  );
 };
 
 export default Navbar;
