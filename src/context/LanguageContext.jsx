fbcb220a01bde4737ccb416a0b07044c2e64b97f
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-09-14 18:09:20 +0200

    basic language

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 export const LanguageContextDefaults = {    
-    value: "en", 
+    value: "hu", 
     setValue: () => { }    
 }
 export const LanguageContext = React.createContext(LanguageContextDefaults);
