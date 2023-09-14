6ab7b2cd403de90b7ddcae5b54645370f1a6db42
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-09-14 12:33:18 +0200

    navHover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext} from 'react';
 import { LanguageContext } from '../context/LanguageContext';
 import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import AnimatedMotion from '../framerMotion/AnimatedMotion';
@@ -15,13 +15,13 @@ const Navbar = () => {
     <Router>
       <AnimatedMotion animationName="navbarAnimatio">
         <div className="flex justify-center p-5">
-          <Link to="/" className="p-2 font-bold border-b-2 border-transparent hover:border-blue-500">
+          <Link to="/" className="p-2 font-bold border-b-2 border-transparent hover:border-blue-700">
             <span>{i18n.text(language, i18n.MAP['navbar-home'])}</span>
           </Link>
-          <Link to="/skills" className="p-2 font-bold border-b-2 border-transparent hover:border-blue-500">
+          <Link to="/skills" className="p-2 font-bold border-b-2 border-transparent hover:border-blue-700">
             <span>{i18n.text(language, i18n.MAP['navbar-skills'])}</span>
           </Link>
-          <Link to="/portfolio" className="p-2 font-bold border-b-2 border-transparent hover:border-blue-500">
+          <Link to="/portfolio" className="p-2 font-bold border-b-2 border-transparent hover:border-blue-700">
             <span>{i18n.text(language, i18n.MAP['navbar-portfolio'])}</span>
           </Link>
         </div>
