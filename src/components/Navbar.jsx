ea93a44938bb3e414647d2c4b3edd57251c8c6ca
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-09-14 10:16:15 +0200

    link-hover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,13 +15,13 @@ const Navbar = () => {
     <Router>
       <AnimatedMotion animationName="navbarAnimatio">
         <div className="flex justify-center p-5">
-          <Link to="/" className={`p-2 hover:border-b-2 font-bold `}>
+          <Link to="/" className="p-2 font-bold border-b-2 border-transparent hover:border-blue-500">
             <span>{i18n.text(language, i18n.MAP['navbar-home'])}</span>
           </Link>
-          <Link to="/skills" className={`p-2 hover:border-b-2 font-bold`}>
+          <Link to="/skills" className="p-2 font-bold border-b-2 border-transparent hover:border-blue-500">
             <span>{i18n.text(language, i18n.MAP['navbar-skills'])}</span>
           </Link>
-          <Link to="/portfolio" className={`p-2 hover:border-b-2 font-bold`}>
+          <Link to="/portfolio" className="p-2 font-bold border-b-2 border-transparent hover:border-blue-500">
             <span>{i18n.text(language, i18n.MAP['navbar-portfolio'])}</span>
           </Link>
         </div>
