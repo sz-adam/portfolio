c1895a0e8f03f4226230991f9361396b99854207
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-10-14 21:01:46 +0200

    projectmodal responsiv

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -13,14 +13,14 @@ const ProjectModal = ({ closeModal, portfolio }) => {
         <div className="z-50 fixed inset-0 flex items-center justify-center p-5 ">
             <div className="absolute inset-0 bg-slate-900 opacity-20"></div>
             <AnimatedMotion animationName="modalAnimations"
-                className="relative bg-slate-700 border w-1/2 h-1/2 max-sm:w-full rounded-2xl max-md:h-full flex text-center max-md:flex-col"
+                className="relative bg-slate-700 border w-2/3 h-2/3 max-sm:w-full rounded-2xl max-md:h-full flex text-center max-md:flex-col"
             >
-                <div className="w-1/2 max-md:w-full max-md:h-[40%] ">
+                <div className="w-1/2 max-md:w-full max-sm:h-[45%] max-md:h-[55%]">
                     <img className="w-full h-full object-cover max-md:rounded-t-2xl md:rounded-l-2xl" src={portfolio.src} alt={portfolio.name} />
                 </div>
-                <div className="flex flex-col justify-center items-center text-center w-full md:w-1/2 p-4">
+                <div className="flex flex-col justify-center items-center text-center w-full  md:w-1/2 p-4">
                     <h1 className="text-white text-2xl font-bold">{portfolio.name}</h1>
-                    <p className="text-gray-200 my-4 md:my-10 text-justify">{portfolio.title}</p>
+                    <p className="text-gray-200 my-4 md:my-10 text-justify ">{portfolio.title}</p>
                     <div className="mt-4 space-x-4">
                         <a href={portfolio.demo}>
                             <button className="text-white">
@@ -33,7 +33,7 @@ const ProjectModal = ({ closeModal, portfolio }) => {
                             </button>
                         </a>
                     </div>
-                    <div className="mt-10">
+                    <div className="mt-10 max-sm:mt-2">
                         <button onClick={closeModal} className="relative inline-flex items-center justify-center p-4 px-10 py-3 border overflow-hidden font-medium transition duration-300 ease-out rounded-full shadow-md group">
                             <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-cyan-700 group-hover:translate-x-0 ease">
                                 <IoMdClose size={25} />
