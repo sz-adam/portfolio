4b0bc638db0c0440a302e3621e82e2bfb1783043
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-10-14 16:33:17 +0200

    delete exit

--- src/framerMotion/AnimatedMotion.jsx
+++ src/framerMotion/AnimatedMotion.jsx
@@ -4,50 +4,42 @@ import { motion } from 'framer-motion';
 const animations = {
   navbarAnimatio: {
     initial: { opacity: 0, y: -35 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -50 },
+    animate: { opacity: 1, y: 0 },   
     transition: { duration: 1 },
   },
   homeImageAnimation: {
     initial: { opacity: 0, scale: 0.8, },
-    animate: { opacity: 1, scale: 1, },
-    exit: { opacity: 0, scale: 0.8, },
+    animate: { opacity: 1, scale: 1, },  
     transition: { duration: 1 },
   },
   homeH1Animation: {
     initial: { opacity: 0, x: -150 },
     animate: { opacity: 1, x: 0 },
-    exit: { opacity: 0, y: -50 },
     transition: { duration: 1.3 },
   },
   homeH3Animation: {
     initial: { opacity: 0, x: -150 },
     animate: { opacity: 1, x: 0 },
-    exit: { opacity: 0, y: -50 },
     transition: { delay: 1, duration: 1.2 },
   },
   homePAnimation: {
     initial: { opacity: 0, x: -150 },
     animate: { opacity: 1, x: 0 },
-    exit: { opacity: 0, y: -50 },
     transition: { delay: 2, duration: 1 },
   },
   homeButtonAnimation: {
     initial: { opacity: 0, x: -150 },
     animate: { opacity: 1, x: 0 },
-    exit: { opacity: 0, y: -50 },
     transition: { delay: 3, duration: 1 },
   },
   skillAnimation: {
     initial: { opacity: 0, y: -100 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: 10 },
+    animate: { opacity: 1, y: 0 },    
     transition: { duration: 1.5, type: "spring", stiffness: 300 },
   },
   textAnimation: {
     initial: { opacity: 0, x: -150 },
     animate: { opacity: 1, x: 0 },
-    exit: { opacity: 0, y: -50 },
     transition: { duration: 1 },
   },
   ItemAnimation: {
@@ -63,7 +55,7 @@ const animations = {
     animate: { opacity: 1, scale: 1, },
     exit: { opacity: 0, scale: 0.8, },
     transition: { duration: 1.5 },
-  },
+  }, 
 }
 
 const AnimatedMotion = ({ animationName, children }) => {
@@ -76,7 +68,7 @@ const AnimatedMotion = ({ animationName, children }) => {
       exit={animation.exit}
       transition={animation.transition}
       variants={animation.variants}
-      whileHover={animation.whileHover}
+      whileHover={animation.whileHover}    
     >
       {children}
     </motion.div>
