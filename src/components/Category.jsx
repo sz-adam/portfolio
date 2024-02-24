3b10eac1fa9fc116d085d1705ab96aabbc589411
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-24 11:27:08 +0100

    delete all category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,32 +1,17 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import AnimatedMotion from "../framerMotion/AnimatedMotion";
-import { LanguageContext } from "../context/LanguageContext";
-
-const i18n = require("../utils/i18n");
 
 function Category({ portfolios, setFilteredPortfolios }) {
-  const { language } = useContext(LanguageContext);
-
-  const plusCategories = [
-    i18n.text(language, i18n.MAP["portfolio-category"]),
+  const [selectedCategory, setSelectedCategory] = useState("React");
+  const unificationCategories = [
     ...new Set(portfolios.map((portfolio) => portfolio.category)),
   ];
 
-  const [selectedCategory, setSelectedCategory] = useState(
-    i18n.text(language, i18n.MAP["portfolio-category"])
-  );
-
   useEffect(() => {
-    if (
-      selectedCategory === i18n.text(language, i18n.MAP["portfolio-category"])
-    ) {
-      setFilteredPortfolios(portfolios);
-    } else {
-      const filtered = portfolios.filter(
-        (portfolio) => portfolio.category === selectedCategory
-      );
-      setFilteredPortfolios(filtered);
-    }
+    const filtered = portfolios.filter(
+      (portfolio) => portfolio.category === selectedCategory
+    );
+    setFilteredPortfolios(filtered);
   }, [selectedCategory]);
 
   const handleCategoryClick = (category) => {
@@ -38,7 +23,7 @@ function Category({ portfolios, setFilteredPortfolios }) {
       animationName="navbarAnimatio"
       className="flex flex-wrap justify-center items-center"
     >
-      {plusCategories.map((category, index) => (
+      {unificationCategories.map((category, index) => (
         <div
           className={`px-2 text-lg cursor-pointer font-bold ${
             selectedCategory === category ? "text-green-500" : ""
