6ceec39456f82787796f9d7908c93ec85ab3ad05
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-24 09:41:12 +0100

    add multi language

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,22 +1,30 @@
-import React, {useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import AnimatedMotion from "../framerMotion/AnimatedMotion";
+import { LanguageContext } from "../context/LanguageContext";
 
+const i18n = require("../utils/i18n");
 
 function Category({ portfolios, setFilteredPortfolios }) {
-
+  const { language } = useContext(LanguageContext);
 
   const plusCategories = [
-    "Összes",
+    i18n.text(language, i18n.MAP["portfolio-category"]),
     ...new Set(portfolios.map((portfolio) => portfolio.category)),
   ];
 
-  const [selectedCategory, setSelectedCategory] = useState("Összes"); 
+  const [selectedCategory, setSelectedCategory] = useState(
+    i18n.text(language, i18n.MAP["portfolio-category"])
+  );
 
-  useEffect(() => {   
-    if (selectedCategory === "Összes") {
+  useEffect(() => {
+    if (
+      selectedCategory === i18n.text(language, i18n.MAP["portfolio-category"])
+    ) {
       setFilteredPortfolios(portfolios);
     } else {
-      const filtered = portfolios.filter((portfolio) => portfolio.category === selectedCategory);
+      const filtered = portfolios.filter(
+        (portfolio) => portfolio.category === selectedCategory
+      );
       setFilteredPortfolios(filtered);
     }
   }, [selectedCategory]);
@@ -26,23 +34,24 @@ function Category({ portfolios, setFilteredPortfolios }) {
   };
 
   return (
-   
-      <AnimatedMotion animationName="navbarAnimatio"  className="flex flex-wrap justify-center items-center">
-        {plusCategories.map((category, index) => (
-          <div
-            className={`px-2 text-lg cursor-pointer font-bold ${
-              selectedCategory === category ? "text-green-500" : ""
-            }`}
-            key={index}
-            onClick={() => handleCategoryClick(category)}
-          >
-            <AnimatedMotion animationName="navbarItemAnimatio">
-          <p>  {category}</p>
+    <AnimatedMotion
+      animationName="navbarAnimatio"
+      className="flex flex-wrap justify-center items-center"
+    >
+      {plusCategories.map((category, index) => (
+        <div
+          className={`px-2 text-lg cursor-pointer font-bold ${
+            selectedCategory === category ? "text-green-500" : ""
+          }`}
+          key={index}
+          onClick={() => handleCategoryClick(category)}
+        >
+          <AnimatedMotion animationName="navbarItemAnimatio">
+            <p> {category}</p>
           </AnimatedMotion>
-          </div>
-        ))}
-      </AnimatedMotion>    
-  
+        </div>
+      ))}
+    </AnimatedMotion>
   );
 }
 
