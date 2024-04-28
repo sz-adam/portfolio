6e18f9a1df3d2400735bbc9df02c3b7792215f88
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-28 12:44:55 +0200

    repair image

--- src/components/HomeInformation.jsx
+++ src/components/HomeInformation.jsx
@@ -1,15 +1,15 @@
-import React, { useContext, useState } from "react";
-import Me4 from "../images/me44.png";
-import { ThemeContext } from "../context/ThemeContext";
-import { LanguageContext } from "../context/LanguageContext";
-import AnimatedMotion from "../framerMotion/AnimatedMotion";
-import { AiOutlineMail } from "react-icons/ai";
-import { BsGithub } from "react-icons/bs";
-import { BsLinkedin } from "react-icons/bs";
-import FlagThemeModal from "../components/FlagThemeModal";
-import { FcSettings } from "react-icons/fc";
-import { AnimatePresence } from "framer-motion";
-const i18n = require("../utils/i18n");
+import React, { useContext, useState } from 'react';
+import Me4 from '../images/me44.png';
+import { ThemeContext } from '../context/ThemeContext';
+import { LanguageContext } from '../context/LanguageContext';
+import AnimatedMotion from '../framerMotion/AnimatedMotion';
+import { AiOutlineMail } from "react-icons/ai"
+import { BsGithub } from "react-icons/bs"
+import { BsLinkedin } from "react-icons/bs"
+import FlagThemeModal from '../components/FlagThemeModal';
+import { FcSettings } from "react-icons/fc"
+import { AnimatePresence } from 'framer-motion';
+const i18n = require('../utils/i18n');
 
 const HomeInformation = () => {
   const { language } = useContext(LanguageContext);
@@ -18,67 +18,56 @@ const HomeInformation = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
+
     <AnimatedMotion animationName="homeImageAnimation">
       <div className="p-3">
         {/* modal ablak */}
-        <button onClick={() => setIsOpen(true)}>
-          {" "}
-          <FcSettings size={42} className="animate-spin " />
-        </button>
+        <button onClick={() => setIsOpen(true)}>  <FcSettings size={42} className="animate-spin " /></button>
         <AnimatePresence>
-          {isOpen && <FlagThemeModal setIsOpen={setIsOpen} />}
+        {isOpen && <FlagThemeModal setIsOpen={setIsOpen} />}
         </AnimatePresence>
       </div>
-      <div className="flex flex-col">
-        <div className="flex justify-center items-center pt-2 m-2">
+      <div className="flex flex-col " >
+        <div className="flex justify-center items-center pt-2" >
           <img
             src={Me4}
             alt="my portfolio"
-            className="w-72 sm:w-96 lg:w-64 xl:w-96 rounded-full  "
+            className="w-72 sm:w-96 lg:w-80 rounded-full "
             style={{ boxShadow: boxShadow }}
           />
         </div>
         <div className="text-center p-5 ">
-          <h1 className="text-4xl p-1 ">
-            {i18n.text(language, i18n.MAP["about-name"])}
-          </h1>
-          <h2 className="text-xl">
-            {i18n.text(language, i18n.MAP["about-title"])}
-          </h2>
+          <h1 className="text-4xl p-1 " >{i18n.text(language, i18n.MAP['about-name'])}</h1>
+          <h2 className="text-xl">{i18n.text(language, i18n.MAP['about-title'])}</h2>
         </div>
 
         {/**link */}
+        <div>
+          <div className="p-3 ">
+            <a href="https://github.com/sz-adam?tab=repositories" className="flex items-center text-center">
+              <BsGithub className="text-xl animate-bounce" />
+              <span className="ml-4">Github</span>
+            </a>
+          </div>
+          <div className="p-3">
+            <a href="mailto:szaboadam255@gmail.com" className="flex items-center text-center">
+              <AiOutlineMail className="text-xl animate-bounce" />
+              <span className="ml-4 ">szaboadam255@gmail.com</span>
+            </a>
+          </div>
+          <div className="p-3" >
+            <a href="https://www.linkedin.com/in/%C3%A1d%C3%A1m-szab%C3%B3-06733527a/" className="flex  items-center text-center">
+              <BsLinkedin className="text-xl animate-bounce" />
+              <span className="ml-4">Linkedin</span>
+            </a>
+          </div>
 
-        <div className="p-3 ">
-          <a
-            href="https://github.com/sz-adam?tab=repositories"
-            className="flex items-center text-center"
-          >
-            <BsGithub className="text-xl animate-bounce" />
-            <span className="ml-4">Github</span>
-          </a>
         </div>
-        <div className="p-3">
-          <a
-            href="mailto:szaboadam255@gmail.com"
-            className="flex items-center text-center"
-          >
-            <AiOutlineMail className="text-xl animate-bounce" />
-            <span className="ml-4 ">szaboadam255@gmail.com</span>
-          </a>
-        </div>
-        <div className="p-3">
-          <a
-            href="https://www.linkedin.com/in/%C3%A1d%C3%A1m-szab%C3%B3-06733527a/"
-            className="flex  items-center text-center"
-          >
-            <BsLinkedin className="text-xl animate-bounce" />
-            <span className="ml-4">Linkedin</span>
-          </a>
-        </div>
+
       </div>
     </AnimatedMotion>
-  );
-};
 
-export default HomeInformation;
+  )
+}
+
+export default HomeInformation
