2c45938f9174793865da63b1eb89af9de8b33c70
Szabó Ádám <112242221+sz-adam@users.noreply.github.com>
2025-07-06 17:11:26 +0200

    new loading

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,17 +1,62 @@
-import { useContext } from 'react'
-import { LanguageContext } from '../context/LanguageContext'
-const i18n = require('../utils/i18n')
+import { useContext } from "react";
+import { motion } from "framer-motion";
+import { LanguageContext } from "../context/LanguageContext";
+const i18n = require("../utils/i18n");
 
-export default function Loading(props) {
-    const { language} = useContext(LanguageContext)
-    return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <div className="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-purple-400 via-blue-500 to-red-400 ">
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 border-2 rounded-full"></div>
-            </div>
-            <span className="text-3xl mt-5 "><h1>{i18n.text(language, i18n.MAP['loading-loading'])}</h1></span>
-        </div>
-    )
+export default function Loading({ count = 5 }) {
+  const { language } = useContext(LanguageContext);
+  const colors = [
+    "bg-[#22238f]",
+    "bg-[#6b45fa]",
+    "bg-[#ca3286]",
+    "bg-[#fe2b49]",
+    "bg-[#fe652d]",
+  ];
+  const loadingText = i18n.text(language, i18n.MAP["loading-loading"]);
+
+  const containerVariants = {
+    initial: {},
+    animate: {
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const dotVariants = {
+    initial: {},
+    animate: {
+      y: [0, -30, 0],
+      transition: {
+        duration: 0.7,
+        repeat: Infinity,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <motion.div
+        variants={containerVariants}
+        initial="initial"
+        animate="animate"
+        className="flex gap-4 h-[100px] items-end"
+      >
+        {Array(count)
+          .fill(null)
+          .map((_, index) => (
+            <motion.div
+              key={index}
+              variants={dotVariants}
+              className={`w-10 h-10 rounded-full ${
+                colors[index % colors.length]
+              }`}
+            />
+          ))}
+      </motion.div>
+      <p className="mt-8 text-4xl font-medium">{loadingText}</p>
+    </div>
+  );
 }
-
-
